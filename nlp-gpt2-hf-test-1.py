9eaec39e804db4ce8f0a7150723d084b0d2749ad
Ángel Fernández Sobrino <fernandezsobrinoangel@gmail.com>
2023-03-22 20:36:27 +0100

    Remove whitespaces

--- nlp-gpt2-hf-test-1.py
+++ nlp-gpt2-hf-test-1.py
@@ -1,93 +1,29 @@
 import pprint
-from transformers import pipeline
+from transformers import pipeline, set_seed
 
-generator = pipeline('text-generation', max_new_tokens=300, model='EleutherAI/gpt-neo-125M')
+generator = pipeline('text-generation', model='gpt2-large')
 
 # Establecer el prompt
 prompt = """
 Añadir 2000 kg de agua y calentar a 35 grados
 
-{
-    "add_water": {
-        "amount": 2000,
-        "unit": "kg"
-    },
-    "temperature": {
-        "value": 35,
-        "unit": "C"
-    },
-    "palas": {
-        "value": 0,
-        "unit": "rpm"
-    },
-    "turrax": {
-        "value": 0,
-        "unit": "rpm"
-    },
-    "marina": {
-        "value": 0,
-        "unit": "rpm"
-    }
-}
+{"add_water":{"amount":2000,"unit":"kg"},"temperature":{"value":35,"unit":"C"},"palas":{"value":0,"unit":"rpm"},"turrax":{"value":0,"unit":"rpm"},"marina":{"value":0,"unit":"rpm"},"time":{"value":0,"unit":"min"},"solids":{"value":0,"unit":"kg","product":""}}
 
 Una vez llenado, encender palas a 1000 rpm y turrax a 400 rpm durante 25 minutos
 
-{
-    "temperature": {
-        "value": 35,
-        "unit": "C"
-    },
-    "palas": {
-        "value": 1000,
-        "unit": "rpm"
-    },
-    "turrax": {
-        "value": 400,
-        "unit": "rpm"
-    },
-    "marina": {
-        "value": 0,
-        "unit": "rpm"
-    },
-    "time": {
-        "value": 25,
-        "unit": "min"
-    }
-}
+{"add_water":{"amount":0,"unit":"0"},"temperature":{"value":35,"unit":"C"},"palas":{"value":1000,"unit":"rpm"},"turrax":{"value":400,"unit":"rpm"},"marina":{"value":0,"unit":"rpm"},"time":{"value":25,"unit":"min"},"solids":{"value":0,"unit":"kg","product":""}}
 
 Encender la marina a 50 rpm y llenar por la boca de solidos con 100 kg de azucar
 
-{
-    "temperature": {
-        "value": 35,
-        "unit": "C"
-    },
-    "palas": {
-        "value": 1000,
-        "unit": "rpm"
-    },
-    "turrax": {
-        "value": 400,
-        "unit": "rpm"
-    },
-    "marina": {
-        "value": 50,
-        "unit": "rpm"
-    },
-    "solids": {
-        "value": 100,
-        "unit": "kg",
-        "product": "azucar"
-    }
-}
+{"add_water":{"amount":0,"unit":"kg"},"temperature":{"value":35,"unit":"C"},"palas":{"value":1000,"unit":"rpm"},"turrax":{"value":400,"unit":"rpm"},"marina":{"value":50,"unit":"rpm"},"time":{"value":0,"unit":"min"},"solids":{"value":100,"unit":"kg","product":"azucar"}}
 
 Al terminar subir la temperatura a 40 grados y poner palar a 1500 rpm, esperar 1 hora
 """
 
 
-
+set_seed(42)
 # Generar texto basado en el prompt
-result = generator(prompt, do_sample=True)
+result = generator(prompt, max_length=1000, num_return_sequences=5)
 
 # Imprimir el resultado generado
 pprint.pp(result)
